72ad3c578e667dce79f56557893fa4548b3607da
Greenfoot5 <greenfoot89@gmail.com>
2024-12-09 19:21:29 +0000

    Fixed build error

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,14 +5,12 @@
 </script>
 
 <svelte:head>
-	<svelte:head>
-		<!-- Be sure to add your image files and un-comment the lines below -->
-		<title>{siteTitle}</title>
-		<meta data-key="description" name="description" content="Aeon's Legends Wiki Thingy">
-		<meta property="og:title" content="Greenpedia - Aeon's Legends" />
-		<meta property="og:description" content="Aeon's Legends Wiki Thingy" />
-		<meta property="og:url" content="https://al.alchemix.dev/" />
-	</svelte:head>
+	<!-- Be sure to add your image files and un-comment the lines below -->
+	<title>{siteTitle}</title>
+	<meta data-key="description" name="description" content="Aeon's Legends Wiki Thingy">
+	<meta property="og:title" content="Greenpedia - Aeon's Legends" />
+	<meta property="og:description" content="Aeon's Legends Wiki Thingy" />
+	<meta property="og:url" content="https://al.alchemix.dev/" />
 </svelte:head>
 
 <Header />
