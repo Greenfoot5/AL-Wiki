753fc8333689cdb2156387b2612b11cf05189efb
Greenfoot5 <greenfoot89@gmail.com>
2024-12-21 07:35:20 +0000

    Added a callout component

---
+++ src/lib/components/Callout.svelte
@@ -0,0 +1,28 @@
+<div class="callout">
+    <slot />
+</div>
+
+<style lang="scss">
+
+    @use '$lib/assets/scss/_vars.scss';
+    .callout {
+        background: var(--nord-overlay);
+        padding: 0.5rem 1rem;
+        width: calc(100% - 2em);
+        max-width: unset;
+        margin: 1rem 0 2rem 0;
+        font-style: italic;
+
+        //border-width: 10px;
+        border: 0 solid;
+        border-left-width: 5px;
+        border-color: var(--nord-frost-0);
+        border-radius: 0.125rem;
+
+        @media (min-width: vars.$mdMin) {
+            width: calc(100% - 4rem);
+            margin: 1rem 0 1rem 0;
+            padding: 0.75rem 1rem;
+        }
+    }
+</style>
