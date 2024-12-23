53ef2e661436309a0402128327f55f037e2c8cb0
Greenfoot5 <greenfoot5@alchemix.dev>
2024-12-23 00:22:52 +0000

    Fixed Github url

--- src/lib/config.js
+++ src/lib/config.js
@@ -4,5 +4,5 @@ export const siteDescription = 'The Greenpedia for Aeon\'s Legends';
 export const siteURL = 'greenfoot5.github.io/greenpedia';
 export const siteLink = 'https://greenfoot5.github.io/greenpedia';
 export const siteAuthor = 'Greenfoot5';
-export const github = 'https://github.com/Greenfoot5/greenpedia';
+export const github = 'https://github.com/Greenfoot5/AL-Wiki';
 //export const patreon = 'https://www.patreon.com/HoeksHome';
