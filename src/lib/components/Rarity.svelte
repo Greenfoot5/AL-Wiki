3a29c5271ed46f8d5c26b8c323644bfb7213a494
Greenfoot5 <harvey@alchemix.dev>
2025-04-09 00:37:18 +0100

    Styled rarity

--- src/lib/components/Rarity.svelte
+++ src/lib/components/Rarity.svelte
@@ -3,11 +3,11 @@
 </script>
 
 {#if rarity.toLowerCase().startsWith("l")}
-    <span style="color: var(--nord-orange)">Legendary</span>
+    <span class="text-amber-500">Legendary</span>
 {:else if rarity.toLowerCase().startsWith("e") || rarity.toLowerCase().startsWith("m")}
-    <span style="color: var(--nord-purple)">Epic</span>
+    <span class="text-indigo-400">Epic</span>
 {:else if rarity.toLowerCase().startsWith("r")}
-    <span style="color: var(--nord-frost-3)">Rare</span>
+    <span class="text-blue-400">Rare</span>
 {:else}
-    <span style="color: var(--nord-text)">Common</span>
+    <span class="text-base">Common</span>
 {/if}
