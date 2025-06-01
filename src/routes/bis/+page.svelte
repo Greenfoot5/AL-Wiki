cf275b9f5976ad534d86bd18ef6683b9f560cb7e
Greenfoot5 <harvey@alchemix.dev>
2025-06-01 16:28:43 +0100

    Setup T4 BIS nav

--- src/routes/bis/+page.svelte
+++ src/routes/bis/+page.svelte
@@ -1,10 +1,16 @@
 <script>
+    import { Accordion } from '@skeletonlabs/skeleton-svelte';
+
     import BISArmourTable from "$lib/components/BISArmourTable.svelte";
     import BISWeaponTable from "$lib/components/BISWeaponTable.svelte";
     import Callout from "$lib/components/Callout.svelte";
     import PreviousTiers from "$lib/components/PreviousTiers.svelte";
+    import { HardHat, Sword, BowArrow, Wand, Shield, Hand, Footprints, Shirt, Swords, Bone, Anvil, Brush } from '@lucide/svelte';
 
     let { data } = $props();
+
+    let opened = $state([]);
+    let weapons = $state([]);
 </script>
 
 
@@ -32,95 +38,147 @@
 
     <Callout variant="error">Many values are inaccurate or missing as the season has just released.</Callout>
 
-    <details>
-        <summary><h2>Head</h2></summary>
-        <BISArmourTable item={data.head.heavy} caption="Heavy" />
-        <div class="hr"></div>
-        <BISArmourTable item={data.head.medium} caption="Medium" />
-        <div class="hr"></div>
-        <BISArmourTable item={data.head.light} caption="Light" />
-    </details>
+    <Accordion {opened} onValueChange={(e) => (value = e.value)} multiple>
+        <Accordion.Item value="head">
+            {#snippet lead()}<HardHat class="h2" size={36} strokeWidth={2.5} />{/snippet}
+            {#snippet control()}<h2 class="h2">Head</h2>{/snippet}
+            <!-- Panel -->
+            {#snippet panel()}
+                <BISArmourTable item={data.head.heavy} caption="Heavy" />
+                <hr class="hr short" />
+                <BISArmourTable item={data.head.medium} caption="Medium" />
+                <hr class="hr short" />
+                <BISArmourTable item={data.head.light} caption="Light" />
+            {/snippet}
+        </Accordion.Item>
+        <hr class="hr" />
+        <Accordion.Item value="shoulders">
+            {#snippet lead()}<Anvil class="h2" size={36} strokeWidth={2.5} />{/snippet}
+            {#snippet control()}<h2 class="h2">Shoulders</h2>{/snippet}
+            <!-- Panel -->
+            {#snippet panel()}
+                <BISArmourTable item={data.shoulders.heavy} caption="Heavy" />
+                <hr class="hr short">
+                <BISArmourTable item={data.shoulders.medium} caption="Medium" />
+                <hr class="hr short">
+                <BISArmourTable item={data.shoulders.light} caption="Light" />
+            {/snippet}
+        </Accordion.Item>
+        <hr class="hr" />
+        <Accordion.Item value="chest">
+            {#snippet lead()}<Shirt class="h2" size={36} strokeWidth={2.5} />{/snippet}
+            {#snippet control()}<h2 class="h2">Chest</h2>{/snippet}
+            <!-- Panel -->
+            {#snippet panel()}
+                <BISArmourTable item={data.chest.heavy} caption="Heavy" />
+                <hr class="hr short">
+                <BISArmourTable item={data.chest.medium} caption="Medium" />
+                <hr class="hr short">
+                <BISArmourTable item={data.chest.light} caption="Light" />
+            {/snippet}
+        </Accordion.Item>
+        <hr class="hr" />
+        <Accordion.Item value="legs">
+            {#snippet lead()}<Bone class="h2" size={36} strokeWidth={2.5} />{/snippet}
+            {#snippet control()}<h2 class="h2">Legs</h2>{/snippet}
+            <!-- Panel -->
+            {#snippet panel()}
+                <BISArmourTable item={data.legs.heavy} caption="Heavy" />
+                <hr class="hr short">
+                <BISArmourTable item={data.legs.medium} caption="Medium" />
+                <hr class="hr short">
+                <BISArmourTable item={data.legs.light} caption="Light" />
+            {/snippet}
+        </Accordion.Item>
+        <hr class="hr" />
+        <Accordion.Item value="hands">
+            {#snippet lead()}<Hand class="h2" size={36} strokeWidth={2.5} />{/snippet}
+            {#snippet control()}<h2 class="h2">Hands</h2>{/snippet}
+            <!-- Panel -->
+            {#snippet panel()}
+                <BISArmourTable item={data.gloves.heavy} caption="Heavy" />
+                <hr class="hr short">
+                <BISArmourTable item={data.gloves.medium} caption="Medium" />
+                <hr class="hr short">
+                <BISArmourTable item={data.gloves.light} caption="Light" />
+            {/snippet}
+        </Accordion.Item>
+        <hr class="hr" />
+        <Accordion.Item value="feet">
+            {#snippet lead()}<Footprints class="h2" size={36} strokeWidth={2.5} />{/snippet}
+            {#snippet control()}<h2 class="h2">Feet</h2>{/snippet}
+            <!-- Panel -->
+            {#snippet panel()}
+                <BISArmourTable item={data.feet.heavy} caption="Heavy" />
+                <hr class="hr short">
+                <BISArmourTable item={data.feet.medium} caption="Medium" />
+                <hr class="hr short">
+                <BISArmourTable item={data.feet.light} caption="Light" />
+            {/snippet}
+        </Accordion.Item>
 
-    <details>
-        <summary><h2>Shoulders</h2></summary>
-        <BISArmourTable item={data.shoulders.heavy} caption="Heavy" />
-        <div class="hr"></div>
-        <BISArmourTable item={data.shoulders.medium} caption="Medium" />
-        <div class="hr"></div>
-        <BISArmourTable item={data.shoulders.light} caption="Light" />
-    </details>
+        <hr class="hr" />
+        <Accordion.Item value="cloak">
+            {#snippet lead()}<Brush class="h2" size={36} strokeWidth={2.5} />{/snippet}
+            {#snippet control()}<h2 class="h2">Cloak</h2>{/snippet}
+            <!-- Panel -->
+            {#snippet panel()}
+                <BISArmourTable item={data.cloak} />
+            {/snippet}
+        </Accordion.Item>
 
-    <details>
-        <summary><h2>Chest</h2></summary>
-        <BISArmourTable item={data.chest.heavy} caption="Heavy" />
-        <div class="hr"></div>
-        <BISArmourTable item={data.chest.medium} caption="Medium" />
-        <div class="hr"></div>
-        <BISArmourTable item={data.chest.light} caption="Light" />
-    </details>
-
-    <details>
-        <summary><h2>Legs</h2></summary>
-        <BISArmourTable item={data.legs.heavy} caption="Heavy" />
-        <div class="hr"></div>
-        <BISArmourTable item={data.legs.medium} caption="Medium" />
-        <div class="hr"></div>
-        <BISArmourTable item={data.legs.light} caption="Light" />
-    </details>
-
-    <details>
-        <summary><h2>Hands</h2></summary>
-        <BISArmourTable item={data.gloves.heavy} caption="Heavy" />
-        <div class="hr"></div>
-        <BISArmourTable item={data.gloves.medium} caption="Medium" />
-        <div class="hr"></div>
-        <BISArmourTable item={data.gloves.light} caption="Light" />
-    </details>
-
-    <details>
-        <summary><h2>Feet</h2></summary>
-        <BISArmourTable item={data.shoes.heavy} caption="Heavy" />
-        <div class="hr"></div>
-        <BISArmourTable item={data.shoes.medium} caption="Medium" />
-        <div class="hr"></div>
-        <BISArmourTable item={data.shoes.light} caption="Light" />
-    </details>
-
-    <details>
-        <summary><h2>Cloak</h2></summary>
-        <BISArmourTable item={data.cloak} />
-    </details>
-
-    <details>
-        <summary><h2>Weapon</h2></summary>
-<!--        <p>All values have been rounded to 2 decimal places. Only whole numbers are used in-game</p>-->
-        <Callout>When picking a weapon bear in mind if the scaling stats match. On each there's a primary & secondary scaling factor to your damage based on str/dex/int/vit. These can significantly affect your damage if you get them right/wrong.</Callout>
-        <details style="margin: 1em 2em;">
-            <summary><h3>Melee</h3></summary>
-            <BISWeaponTable item={data.melee.sword} caption="Sword (one-handed)" />
-            <div class="hr"></div>
-            <BISWeaponTable item={data.melee.axe} caption="Axe (one-handed)" />
-            <div class="hr"></div>
-            <BISWeaponTable item={data.melee.greatsword} caption="GreatSword (two-handed)" />
-            <div class="hr"></div>
-            <BISWeaponTable item={data.melee.greataxe} caption="GreatAxe (two-handed)" />
-        </details>
-
-        <details style="margin: 1em 2em;">
-            <summary><h3>Bow</h3></summary>
-            <BISWeaponTable item={data.bow.bow} caption="Bow (two-handed)" />
-        </details>
-
-        <details style="margin: 1em 2em;">
-            <summary><h3>Staff</h3></summary>
-            <BISWeaponTable item={data.staff.staff} caption="Staff (two-handed)" />
-        </details>
-
-        <details style="margin: 1em 2em;">
-            <summary><h3>Shield</h3></summary>
-            <BISArmourTable item={data.shield} caption="Shield (off-hand only)"/>
-        </details>
-    </details>
+        <hr class="hr" />
+        <Accordion.Item value="weapons">
+            {#snippet lead()}<Swords class="h2" size={36} strokeWidth={2.5} />{/snippet}
+            {#snippet control()}<h2 class="h2">Weapons</h2>{/snippet}
+            <!-- Panel -->
+            {#snippet panel()}
+                <Accordion {opened} onValueChange={(e) => (value = e.value)} multiple>
+                    <Accordion.Item value="Melee">
+                        {#snippet lead()}<Sword class="h4" size={24} strokeWidth={2.5} />{/snippet}
+                        {#snippet control()}<h4 class="h4">Melee</h4>{/snippet}
+                        <!-- Panel -->
+                        {#snippet panel()}
+                            <BISWeaponTable item={data.melee.sword} caption="Sword (one-handed)" />
+                            <hr class="hr short">
+                            <BISWeaponTable item={data.melee.axe} caption="Axe (one-handed)" />
+                            <hr class="hr short">
+                            <BISWeaponTable item={data.melee.greatsword} caption="GreatSword (two-handed)" />
+                            <hr class="hr short">
+                            <BISWeaponTable item={data.melee.greataxe} caption="GreatAxe (two-handed)" />
+                        {/snippet}
+                    </Accordion.Item>
+                    <hr class="hr" />
+                    <Accordion.Item value="Bow">
+                        {#snippet lead()}<BowArrow class="h4" size={24} strokeWidth={2.5} />{/snippet}
+                        {#snippet control()}<h2 class="h4">Bow</h2>{/snippet}
+                        <!-- Panel -->
+                        {#snippet panel()}
+                            <BISWeaponTable item={data.bow.bow} caption="Bow (two-handed)" />
+                        {/snippet}
+                    </Accordion.Item>
+                    <hr class="hr" />
+                    <Accordion.Item value="Staff">
+                        {#snippet lead()}<Wand class="h4" size={24} strokeWidth={2.5} />{/snippet}
+                        {#snippet control()}<h2 class="h4">Staff</h2>{/snippet}
+                        <!-- Panel -->
+                        {#snippet panel()}
+                            <BISWeaponTable item={data.staff.staff} caption="Staff (two-handed)" />
+                        {/snippet}
+                    </Accordion.Item>
+                    <hr class="hr" />
+                    <Accordion.Item value="Shield">
+                        {#snippet lead()}<Shield class="h4" size={24} strokeWidth={2.5} />{/snippet}
+                        {#snippet control()}<h2 class="h4">Shield</h2>{/snippet}
+                        <!-- Panel -->
+                        {#snippet panel()}
+                            <BISWeaponTable item={data.shield} caption="Shield (off-hand only)" />
+                        {/snippet}
+                    </Accordion.Item>
+                </Accordion>
+            {/snippet}
+        </Accordion.Item>
+    </Accordion>
 </div>
 
 <style lang="scss">
@@ -128,31 +186,16 @@
         margin: 2em;
     }
 
-    :is(h2, h3) {
-        display: inline;
-    }
-
-    details {
-        margin: 1em 0;
-        overflow-x: auto;
-    }
-
-    summary {
-        margin: 1rem 0;
-        line-height: 1.1;
-        font-family: var(--accentFont), serif;
-        font-weight: bold;
-        font-size: 1.8rem;
-        color: var(--nord-highlight);
-    }
-
-    .hr {
+    hr {
         content: '';
         height: 0.1em;
         background: var(--nord-frost-1);
-        width: 8em;
         justify-content: center;
         margin: 1em auto;
         bottom: 0;
+    }
+
+    .short {
+        width: 8em;
     }
 </style>
