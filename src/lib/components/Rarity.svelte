a913cf903b20e8262c4b1a6771128229de0fbb0d
Greenfoot5 <harvey@alchemix.dev>
2025-06-01 17:24:43 +0100

    Removed badge class from rarity as it didn't look good inline

--- src/lib/components/Rarity.svelte
+++ src/lib/components/Rarity.svelte
@@ -3,11 +3,11 @@
 </script>
 
 {#if rarity.toLowerCase().startsWith("l")}
-    <span class="badge text-amber-500">Legendary</span>
+    <span class="text-amber-500">Legendary</span>
 {:else if rarity.toLowerCase().startsWith("e") || rarity.toLowerCase().startsWith("m")}
-    <span class="badge text-indigo-400">Epic</span>
+    <span class="text-indigo-400">Epic</span>
 {:else if rarity.toLowerCase().startsWith("r")}
-    <span class="badge text-blue-400">Rare</span>
+    <span class="text-blue-400">Rare</span>
 {:else}
     <span class="text-base">Common</span>
 {/if}
