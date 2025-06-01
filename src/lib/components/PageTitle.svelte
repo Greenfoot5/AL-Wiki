f7c6aa71d00bf2d248f43055e0a38103b7ecd57f
Greenfoot5 <harvey@alchemix.dev>
2025-06-01 17:29:37 +0100

    Added page title component for consistent page title

---
+++ src/lib/components/PageTitle.svelte
@@ -0,0 +1,12 @@
+<script>
+    const { title, subtitle } = $props();
+</script>
+
+<div class="text-center">
+    <h1 class="h1">{title}</h1>
+    {#if subtitle}
+        <p class="preset-typo-subtitle">
+            {subtitle}
+        </p>
+    {/if}
+</div>
