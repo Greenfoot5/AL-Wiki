460a1e0fd04b5a33974a77c3fb286cbf3a60f3c8
Greenfoot5 <harvey@alchemix.dev>
2025-06-01 20:46:54 +0100

    Updated embed for BIS

--- src/routes/bis/+page.svelte
+++ src/routes/bis/+page.svelte
@@ -14,15 +14,15 @@
 <svelte:head>
     <!-- Be sure to add your image files and un-comment the lines below -->
     <title>Best in Slot</title>
-    <meta data-key="description" name="description" content="A list of the best stat rolls for each T4 item drop in the game.">
+    <meta data-key="description" name="description" content="A collection of the best stat rolls (drop stats) for each item tier in the game.">
     <meta property="og:title" content="Aeon's Legends - Best in Slot" />
-    <meta property="og:description" content="A list of the best stat rolls for each T4 item drop in the game." />
+    <meta property="og:description" content="A collection of the best stat rolls (drop stats) for each item tier in the game." />
     <meta property="og:url" content="https://al.alchemix.dev/bis" />
 </svelte:head>
 
 <div class="body">
     <PageTitle title="Best In Slot">
-        This is a list of the best stat rolls (drop stats) for each item tier in the game.
+        A list of the best stat rolls (drop stats) for each item tier in the game.
         Unknown stats are replaced with <span class="text-surface-600-400">???</span> and stats with <span class="text-warning-600-400">?</span> after require confirmation.
         Please let Greenfoot5 know either in-game or via Discord if you've got any missing data, or if you feel some data is wrong!
     </PageTitle>
