496c85d176117c0c731df780015a9b52acb66fe8
Greenfoot5 <harvey@alchemix.dev>
2025-06-01 20:45:34 +0100

    Delete BIS/T3 page as it's now in BIS

--- src/routes/bis/t3/+page.server.js
+++
@@ -1,7 +0,0 @@
-import {parse} from "smol-toml";
-
-export const load = async ({ fetch }) => {
-
-    const got = await fetch("/bis_t3.toml").then((response) => response.text());
-    return parse(got);
-}
