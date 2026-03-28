6f006d4e7fd3f17de951bdf05f6ad9ec4c27d9cf
Greenfoot5 <harvey@alchemix.dev>
2026-03-28 12:02:08 +0000

    Adjusted vertical alignment of loader

--- src/lib/components/Loader.svelte
+++ src/lib/components/Loader.svelte
@@ -10,25 +10,27 @@ THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR I
     let { selected } = $props();
 </script>
 
-{#if selected === "circle"}
-    <div class="loader">
-        <svg viewBox="0 0 80 80">
-            <circle r="32" cy="40" cx="40"></circle>
-        </svg>
-    </div>
-{:else if selected === "triangle"}
-    <div class="loader triangle">
-        <svg viewBox="0 0 86 80">
-            <polygon points="43 8 79 72 7 72"></polygon>
-        </svg>
-    </div>
-{:else}
-    <div class="loader">
-        <svg viewBox="0 0 80 80">
-            <rect height="64" width="64" y="8" x="8"></rect>
-        </svg>
-    </div>
-{/if}
+<div class="md:mt-2">
+    {#if selected === "circle"}
+        <div class="loader">
+            <svg viewBox="0 0 80 80">
+                <circle r="32" cy="40" cx="40"></circle>
+            </svg>
+        </div>
+    {:else if selected === "triangle"}
+        <div class="loader triangle">
+            <svg viewBox="0 0 86 80">
+                <polygon points="43 8 79 72 7 72"></polygon>
+            </svg>
+        </div>
+    {:else}
+        <div class="loader">
+            <svg viewBox="0 0 80 80">
+                <rect height="64" width="64" y="8" x="8"></rect>
+            </svg>
+        </div>
+    {/if}
+</div>
 
 <style lang="scss">
     .loader {
